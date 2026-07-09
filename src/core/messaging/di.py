0a35d36aff9caf826993458f80fcdb50751c79ac
Ivan Aleksandrov <ivan_aleksandrov2017@mail.ru>
2026-07-10 00:42:08 +0700

    добавлен воркер публикатора и создан для него отдельный di контейнер

--- src/core/messaging/di.py
+++ src/core/messaging/di.py
@@ -1,11 +1,45 @@
+from collections.abc import AsyncIterable
+
 from dishka import Provider, Scope, provide
 from faststream.rabbit import RabbitBroker
 
 from core.config import Settings
 from core.messaging.broker import create_broker
+from core.messaging.events import payments_dlq, payments_exchange, payments_new_queue, payments_retry_queue
+from payments.application.interfaces.event_publisher import IEventPublisher
+from payments.application.interfaces.outbox_repository import IOutboxRepository
+from payments.application.interfaces.unit_of_work import IUnitOfWork
+from payments.application.outbox_publisher_service import OutboxPublisherService
+from payments.infrastructure.messaging.event_publisher import EventPublisher
 
 
-class MessagingProvider(Provider):
+class OutboxPublisherProvider(Provider):
     @provide(scope=Scope.APP)
-    def broker(self, settings: Settings) -> RabbitBroker:
-        return create_broker(settings=settings)
+    async def broker(self, settings: Settings) -> AsyncIterable[RabbitBroker]:
+        broker = create_broker(settings=settings)
+        await broker.start()
+        exchange = await broker.declare_exchange(payments_exchange)
+        payments_queue = await broker.declare_queue(payments_new_queue)
+        retry_queue = await broker.declare_queue(payments_retry_queue)
+        dlq_queue = await broker.declare_queue(payments_dlq)
+        await payments_queue.bind(exchange, routing_key=payments_new_queue.routing())
+        await retry_queue.bind(exchange, routing_key=payments_retry_queue.routing())
+        await dlq_queue.bind(exchange, routing_key=payments_dlq.routing())
+
+        try:
+            yield broker
+        finally:
+            await broker.stop()
+
+    @provide(scope=Scope.REQUEST)
+    def event_publisher(self, broker: RabbitBroker) -> IEventPublisher:
+        return EventPublisher(broker=broker)
+
+    @provide(scope=Scope.REQUEST)
+    def outbox_publisher_service(
+        self,
+        unit_of_work: IUnitOfWork,
+        outbox_repository: IOutboxRepository,
+        event_publisher: IEventPublisher,
+    ) -> OutboxPublisherService:
+        return OutboxPublisherService(uow=unit_of_work, outbox_repo=outbox_repository, event_publisher=event_publisher)
--- src/entrypoints/container.py
+++ src/entrypoints/container.py
@@ -2,7 +2,7 @@ from dishka import AsyncContainer, make_async_container
 
 from core.db.di import DbProvider
 from core.di import SettingsProvider
-from core.messaging.di import MessagingProvider
+from core.messaging.di import OutboxPublisherProvider
 from payments.di import PaymentProvider
 
 
@@ -10,6 +10,14 @@ def create_container() -> AsyncContainer:
     return make_async_container(
         SettingsProvider(),
         DbProvider(),
-        MessagingProvider(),
+        PaymentProvider(),
+    )
+
+
+def create_outbox_publisher_container() -> AsyncContainer:
+    return make_async_container(
+        SettingsProvider(),
+        DbProvider(),
+        OutboxPublisherProvider(),
         PaymentProvider(),
     )
