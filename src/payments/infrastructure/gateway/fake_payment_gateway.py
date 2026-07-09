d43015c074969013509f86c60bc5f8298ba6f657
Ivan Aleksandrov <ivan_aleksandrov2017@mail.ru>
2026-07-10 05:42:12 +0700

    добавлено уведомление клиента через webhook

--- src/payments/infrastructure/gateway/fake_payment_gateway.py
+++ src/payments/infrastructure/gateway/fake_payment_gateway.py
@@ -3,26 +3,25 @@
 import asyncio
 import random
 
+from core.config import Settings
 from payments.application.interfaces.payment_gateway import IPaymentGateway
 from payments.domain.entities.payment_entity import PaymentEntity
 from payments.domain.value_objects import PaymentGatewayResult
 
 
 class FakePaymentGateway(IPaymentGateway):
-    def __init__(
-        self,
-        success_rate: float = 0.9,
-        min_delay_seconds: float = 2,
-        max_delay_seconds: float = 5,
-    ) -> None:
-        self.success_rate = success_rate
-        self.min_delay_seconds = min_delay_seconds
-        self.max_delay_seconds = max_delay_seconds
+    def __init__(self, settings: Settings) -> None:
+        self.settings = settings
 
     async def process(self, payment_entity: PaymentEntity) -> PaymentGatewayResult:
-        await asyncio.sleep(random.uniform(self.min_delay_seconds, self.max_delay_seconds))
+        await asyncio.sleep(
+            random.uniform(
+                self.settings.PAYMENT_GATEWAY_MIN_DELAY_SECONDS,
+                self.settings.PAYMENT_GATEWAY_MAX_DELAY_SECONDS,
+            )
+        )
 
-        if random.random() < self.success_rate:
+        if random.random() < self.settings.PAYMENT_GATEWAY_SUCCESS_RATE:
             return PaymentGatewayResult.SUCCEEDED
 
         return PaymentGatewayResult.FAILED
